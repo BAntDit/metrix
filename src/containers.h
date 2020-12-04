ea2f8e4a6c81e31a7d7af6e96fc41eefdadbf702
BAntDit <bantdit@example.com>
2020-12-04 22:18:55 +0300

    feat(containers): makes possible to build type_list of arrays of type T with sizes from 1 to N.

--- src/containers.h
+++ src/containers.h
@@ -6,6 +6,17 @@
 #define EASY_MP_CONTAINERS_H
 
 namespace easy_mp {
+namespace _internal {
+template<typename T, typename MaxSize>
+struct _array_list;
+
+template<typename T, size_t... idx>
+struct _array_list<T, std::index_sequence<idx...>>
+{
+    using type = type_list<std::array<T, idx + 1>...>;
+};
+}
+
 template<typename T, typename = void>
 struct is_iterable : std::false_type
 {};
@@ -28,6 +39,15 @@ struct is_contiguous<T, std::void_t<decltype(std::declval<T>().data())>> : std::
 
 template<typename T>
 inline constexpr bool is_contiguous_v = is_contiguous<T>::value;
+
+template<typename T, size_t Size>
+struct array_list
+{
+    using type = typename _internal::_array_list<T, std::make_index_sequence<Size>>::type;
+};
+
+template<typename T, size_t Size>
+using array_list_t = typename array_list<T, Size>::type;
 }
 
 #endif // EASY_MP_CONTAINERS_H
