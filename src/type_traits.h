7c6b34da3c9ab3b94742df01524eeb8b34f293e3
asedov <asedov@saber.games>
2025-09-03 19:25:40 +0400

    feat(type_traits): adds member functions traits

--- src/type_traits.h
+++ src/type_traits.h
@@ -6,6 +6,7 @@
 #define METRIX_TYPE_TRAITS_H
 
 #include <type_traits>
+#include "type_list.h"
 
 namespace metrix {
 template<typename T, template<typename...> typename Primary>
@@ -18,6 +19,26 @@ struct is_specialization_of<Primary<Args...>, Primary> : std::true_type
 
 template<typename T, template<typename...> typename Primary>
 inline constexpr bool is_specialization_of_v = is_specialization_of<T, Primary>::value;
+
+template<typename MemberFunctionPtr>
+struct member_function_traits;
+
+template<typename Ret, class Class, typename... Args>
+struct member_function_traits<Ret (Class::*)(Args&&...)>
+{
+	using return_type = Ret;
+	using argument_type_list = type_list<Args...>;
+	using class_type = Class;
+};
+
+template<typename MemberFunctionPtr>
+using member_function_return_type_t = typename member_function_traits<MemberFunctionPtr>::return_type; 
+
+template<typename MemberFunctionPtr>
+using member_function_argument_type_list_t = typename member_function_traits<MemberFunctionPtr>::argument_type_list; 
+
+template<typename MemberFunctionPtr>
+using member_function_class_type_t = typename member_function_traits<MemberFunctionPtr>::class_type; 
 }
 
 #endif // METRIX_TYPE_TRAITS_H
